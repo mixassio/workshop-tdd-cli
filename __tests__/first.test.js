6c7ccbda3388765672a48cf26b9d276be697de8f
mixassio <mixail.bondarev@gmail.com>
2018-10-13 14:35:10 +0300

    mock

--- __tests__/first.test.js
+++ __tests__/first.test.js
@@ -5,14 +5,22 @@ import exchange from '../src/index';
 nock.disableNetConnect();
 describe('Test function', () => {
   const data = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/cbrf.json', 'utf8'));
-  const truResponse = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/trueResponse.json', 'utf8'));
+  const trueResponse = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/trueResponse.json', 'utf8'));
 
-  it('check simple situate', async () => {
+  it('check with dependency inversion', async () => {
     const httpLib = () => ({
       status: 200,
       data,
     });
     const answer = await exchange(httpLib);
-    expect(answer).toEqual(truResponse);
+    expect(answer).toEqual(trueResponse);
+  });
+  it('check with mock', async () => {
+    nock('https://www.cbr-xml-daily.ru')
+      .get('/daily_json.js')
+      .reply(200, { data });
+    const answer = await exchange();
+    console.log(answer);
+    expect(answer).toEqual(trueResponse);
   });
 });
